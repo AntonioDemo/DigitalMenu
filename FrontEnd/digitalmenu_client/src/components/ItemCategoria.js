58feae826762dfb51ab2f099ce62fe3a14542a86
AntonioDemo <47781733+AntonioDemo@users.noreply.github.com>
2021-01-04 16:57:57 +0100

    improved component itemCategoria layout

--- FrontEnd/digitalmenu_client/src/components/ItemCategoria.js
+++ FrontEnd/digitalmenu_client/src/components/ItemCategoria.js
@@ -6,18 +6,25 @@ function ItemCategoria(props) {
         <Card
             onClick={() => (alert("cioa"))}
             hoverable
-            style={{ marginBottom: "10px", marginTop: "10px", marginRight: "10px", marginLeft: "5px" }}>
-            <Row>
-                <Col span={18} push={6}>
-                    <p>Testo</p>
-                </Col>
-                <Col span={6} pull={18}>
+            style={{ margin:"5px" }}
+            bodyStyle={{padding:"10px"}}
+            >
+
+            <Row style={{ margin:"0px", padding:"0px" }} >
+                <Col 
+                style={{width:"100%"}} >
                     <img src={"https://i0.wp.com/www.sicilianicreativiincucina.it/wp-content/uploads/2018/06/linguine-con-vongole.jpg?fit=700%2C686&ssl=1"}
                         style={{
                             width: "90%", display: "block", marginLeft: "auto",
-                            marginRight: "auto", marginTop: "20px"
+                            marginRight: "auto"
                         }}></img>
                 </Col>
+             </Row>
+             <Row>
+                <Col style={{margin:"auto"}} >
+                    <h2>Categoria</h2>
+                </Col>
+               
             </Row>
         </Card>
     );
