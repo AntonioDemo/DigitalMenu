0ee2b3636720e891610607d78aeb43c790682347
AntonioDemo <47781733+AntonioDemo@users.noreply.github.com>
2021-01-05 17:12:44 +0100

    test axios

--- FrontEnd/digitalmenu_client/src/services/DigitalMenuService.js
+++ FrontEnd/digitalmenu_client/src/services/DigitalMenuService.js
@@ -1,19 +1,32 @@
 import axios from 'axios'
 
-
 const IP = "127.0.0.1"
 
-const URL = "http://" + IP + ":8080/";
+//const URL = "http://" + IP + ":8080/";
 
+const URL = "https://jsonplaceholder.typicode.com/"
 class DigitalMenuService {
 
-
     getProva() {
-        return axios.get(URL + "Prova");
+        return axios.get(URL + "todos/1");
     }
 
     getAllPiattiByCategoria() {
         return axios.get(URL + "GestionePiatti")
+    }
+
+    // SEND
+    inviaProva()
+    {
+        return axios({
+            method: 'post',
+            url: 'https://jsonplaceholder.typicode.com/posts',
+            data: {
+                title: 'foo',
+                body: 'bar',
+                userId: 1
+            }
+        });
     }
 
 }
--- FrontEnd/digitalmenu_client/src/view/VistaMenu.js
+++ FrontEnd/digitalmenu_client/src/view/VistaMenu.js
@@ -1,36 +1,33 @@
 import React from 'react';
-//import ProvaCarousel from '../components/ProvaCarousel';
-//import VerticalCarousel from '../components/VerticalCarousel';
-//import ItemOrdine from '../components/ItemOrdine';
-//import DettaglioPiatto from '../components/DettaglioPiatto';
-import { Layout } from 'antd';
+// import ProvaCarousel from '../components/ProvaCarousel'; import
+// VerticalCarousel from '../components/VerticalCarousel'; import ItemOrdine
+// from '../components/ItemOrdine'; import DettaglioPiatto from
+// '../components/DettaglioPiatto';
+import {Layout} from 'antd';
 import ItemCategoria from '../components/ItemCategoria';
 import ItemCarrello from '../components/ItemCarrello';
 import ItemProdotto from '../components/ItemProdotto';
+import DigitalMenuService from '../services/DigitalMenuService';
 //import Temp0Service from '../services/Temp0Service';
-const { Header, Footer, Sider, Content } = Layout;
+const {Header, Footer, Sider, Content} = Layout;
 
-
-
-/*
 function Categoria() {
 
-
-    Service
+    DigitalMenuService
         .getProva()
         .then((res) => {
             console.log(res.data);
         });
 
+    //SEND
+    DigitalMenuService
+        .inviaProva()
+        .then((response) => response.data)
+        .then((json) => console.log(json));
 
-       // console.log(Service.getAllPiattiByCategoria())
-
-    return (
-        true
-    );
-
+    return (true);
 };
-*/
+
 function VistaMenu() {
     return (
         <Layout style={{
@@ -38,20 +35,22 @@ function VistaMenu() {
         }}>
             <Header
                 style={{
-                    padding: "0",
-                    height: "auto"
-                }}>
-                
-            </Header>
+                padding: "0",
+                height: "auto"
+            }}></Header>
             <Layout>
-                
-                <Sider  width={"15%"}  style={{ overflow: 'auto'}} >
-                   <ItemCategoria/>
-                   <ItemCategoria/>
-                   <ItemCategoria/>
-                   <ItemCategoria/>
-                   <ItemCategoria/>
-                   <ItemCategoria/>
+                <Categoria/>
+                <Sider
+                    width={"15%"}
+                    style={{
+                    overflow: 'auto'
+                }}>
+                    <ItemCategoria/>
+                    <ItemCategoria/>
+                    <ItemCategoria/>
+                    <ItemCategoria/>
+                    <ItemCategoria/>
+                    <ItemCategoria/>
                     <ItemCategoria/>
                     <ItemCategoria/>
                     <ItemCategoria/>
@@ -61,39 +60,50 @@ function VistaMenu() {
                     <ItemCategoria/>
                     <ItemCategoria/>
 
-                
                 </Sider>
-                <Content style={{ overflow: 'auto'}} >
-                 <ItemProdotto/>
-                  <ItemProdotto/>
-                   <ItemProdotto/>
+                <Content style={{
+                    overflow: 'auto'
+                }}>
                     <ItemProdotto/>
-                  <ItemProdotto/>
-                   <ItemProdotto/> <ItemProdotto/>
-                  <ItemProdotto/>
-                   <ItemProdotto/> <ItemProdotto/>
-                  <ItemProdotto/>
-                   <ItemProdotto/> <ItemProdotto/>
-                  <ItemProdotto/>
-                   <ItemProdotto/> <ItemProdotto/>
-                  <ItemProdotto/>
-                   <ItemProdotto/> <ItemProdotto/>
-                  <ItemProdotto/>
-                   <ItemProdotto/> <ItemProdotto/>
-                  <ItemProdotto/>
-                   <ItemProdotto/> <ItemProdotto/>
-                  <ItemProdotto/>
-                   <ItemProdotto/>
+                    <ItemProdotto/>
+                    <ItemProdotto/>
+                    <ItemProdotto/>
+                    <ItemProdotto/>
+                    <ItemProdotto/>
+                    <ItemProdotto/>
+                    <ItemProdotto/>
+                    <ItemProdotto/>
+                    <ItemProdotto/>
+                    <ItemProdotto/>
+                    <ItemProdotto/>
+                    <ItemProdotto/>
+                    <ItemProdotto/>
+                    <ItemProdotto/>
+                    <ItemProdotto/>
+                    <ItemProdotto/>
+                    <ItemProdotto/>
+                    <ItemProdotto/>
+                    <ItemProdotto/>
+                    <ItemProdotto/>
+                    <ItemProdotto/>
+                    <ItemProdotto/>
+                    <ItemProdotto/>
+                    <ItemProdotto/>
+                    <ItemProdotto/>
+                    <ItemProdotto/>
                 </Content>
-                
+
             </Layout>
-            <Footer  style={{height: "25vh",backgroundColor: "orange",padding:"5px"}} >
+            <Footer
+                style={{
+                height: "25vh",
+                backgroundColor: "orange",
+                padding: "5px"
+            }}>
                 <ItemCarrello/>
             </Footer>
         </Layout>
     );
 };
-
-
 
 export default VistaMenu;
