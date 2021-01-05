4ba688f2b401b1ce79e989806544144a57a1dc85
AntonioDemo <47781733+AntonioDemo@users.noreply.github.com>
2021-01-05 01:25:49 +0100

    impreved layout component itemCarrello

--- FrontEnd/digitalmenu_client/src/components/ItemCarrello.js
+++ FrontEnd/digitalmenu_client/src/components/ItemCarrello.js
@@ -1,25 +1,117 @@
 import React from 'react';
-import { Row, Col, Card } from 'antd';
+import {Row, Col, Card, Button} from 'antd';
+import {Layout} from 'antd';
+const {Header, Footer, Sider, Content} = Layout;
+
+function RigaProdotto(props) {
+    return (
+        <Row
+            style={{
+            backgroundColor: "grey",
+            padding: "5px"
+        }}>
+            <Col flex="auto">Nome prodotto</Col>
+            <Col flex="100px">Quantità: n</Col>
+            <Col flex="100px">Prezzo: n</Col>
+            <Col flex="100px">imgg</Col>
+        </Row>
+    );
+}
 
 function ItemCarrello(props) {
     return (
-        <Row
-            style={{ width: "100%", height:"100%" }}>
-            
-            <Row
-             style={{ width: "100%", height:"100%" }}>
-                <Col  style={{ width:"80%",  backgroundColor:"blue" }} >
-                    <p>piatto N</p>
-                    <p>Prezzo €</p>
-                    <p>stato</p>
-                </Col>
+        <Layout style={{
+            height: "100%"
+        }}>
+            <div
+                style={{
+                height: "auto",
+                padding: "0px",
+                margin: "0px",
+                backgroundColor: "red"
+            }}>
+                <h2
+                    style={{
+                    margin: "0px",
+                    padding: "0px"
+                }}>Carrello</h2>
+            </div>
+            <Layout>
+                <Layout>
+                    <Content
+                        style={{
+                        height: "100%",
+                        backgroundColor: "green"
+                    }}>
+                        <Row
+                            style={{
+                            width: "100%",
+                            height: "100%"
+                        }}>
+                            <Col
+                                style={{
+                                width: "80%",
+                                height: "100%",
+                                backgroundColor: "red",
+                                overflow: 'auto'
+                            }}>
+                                <RigaProdotto/>
+                                <RigaProdotto/>
+                                <RigaProdotto/>
+                                <RigaProdotto/>
+                                <RigaProdotto/>
+                                <RigaProdotto/>
+                                <RigaProdotto/>
+                                <RigaProdotto/>
+                                <RigaProdotto/>
+                                <RigaProdotto/>
+                                <RigaProdotto/>
+                                <RigaProdotto/>
+                                <RigaProdotto/>
 
-                <Col  style={{width:"20%",backgroundColor:"red" }} >
-                        
-                </Col>
-               
-            </Row>
-        </Row>
+                            </Col>
+
+                            <Col
+                                style={{
+                                width: "20%",
+                                backgroundColor: "yellow"
+                            }}>
+                                <Row
+                                    style={{
+                                    width: "100%",
+                                    height: "100%"
+                                }}>
+                                    <Col
+                                        style={{
+                                        width: "100%",
+                                        height:"50%",
+                                        fontSize:"24px"
+                                    }}>
+                                        <h1>Totale:</h1>
+                                        <h1>100,00€</h1>
+                                    </Col>
+
+                                    <Col
+                                        style={{
+                                        width: "100%",
+                                         height:"50%"
+                                    }}>
+                                        <Button
+                                            type="primary"
+                                            style={{
+                                            width: "90%",
+                                            height: "90%"
+                                        }}>PAGA</Button>
+                                    </Col>
+                                </Row>
+                            </Col>
+
+                        </Row>
+                    </Content>
+                </Layout>
+            </Layout>
+        </Layout>
+
     );
 }
 
