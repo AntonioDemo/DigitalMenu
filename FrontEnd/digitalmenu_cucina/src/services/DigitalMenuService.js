09d73dfb87583b9cee48f4ddaccf008af414f0fd
AntonioDemo <47781733+AntonioDemo@users.noreply.github.com>
2021-01-15 17:30:09 +0100

    init frontend cucina

---
+++ FrontEnd/digitalmenu_cucina/src/services/DigitalMenuService.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const IP = "localhost";
+
+const URL = "http://" + IP + ":8080/DigitalMenu_Server/";
+
+class DigitalMenuService {
+  getAllProdottiByCategoria(id) {
+    return axios.get(URL + "GestioneCategoria", {
+      params: {
+        id: id,
+      },
+    });
+  }
+
+  getProdotto(id) {
+    return axios.get(URL + "GestioneProdotto", {
+      params: {
+        idP: id,
+      },
+    });
+  }
+
+  getCategorie() {
+    return axios.post(URL + "VisualizzaCat", {
+      params: {
+        _limit: 10,
+      },
+    });
+  }
+
+  /* SEND*/
+
+  inviaProva(json, id) {
+    axios({
+      method: "post",
+      url: URL + "GestioneOrdine?idPagamento=" + id,
+      headers: {},
+      data: json,
+    });
+  }
+}
+export default new DigitalMenuService();
